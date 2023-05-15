2413df806b7322349011b82f3d39da52b58fa642
Dev-Sharma <sharma.dev.work@gmail.com>
2023-05-15 15:00:57 +0530

    class for new leetcode problem

---
+++ CreateMaximumNumber.java
@@ -0,0 +1,14 @@
+public class CreateMaximumNumber {
+
+    /*
+    nums1 = 3, 4, 6, 5
+    nums2 = 9, 1, 2, 5, 8, 3
+    k = 5
+    */
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+
+        System.out.println("HI");
+        return new int[]{0, 0};
+    }
+
+}
